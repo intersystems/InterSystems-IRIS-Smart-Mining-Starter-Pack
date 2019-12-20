81868a300509deed42717cc0524abb5aeafa1e67
Andres Ulloa <andresjk99@gmail.com>
2019-12-20 17:27:29 -0300

    added quotes to the PWD

--- run.sh
+++ run.sh
@@ -27,9 +27,6 @@ then
     docker-compose rm -f
     docker-compose up --remove-orphans
 else
-    docker run --rm --init --name oee \
-    -v $PWD/iris-analytics:/tmp/iris-analytics \
-    -p 51773:51773 -p 52773:52773 \
-    ${DOCKER_REPO}:oee-version-${VERSION}
+    docker run --rm --init --name oee -v "$PWD/iris-analytics":/tmp/iris-analytics -p 51773:51773 -p 52773:52773 ${DOCKER_REPO}:oee-version-${VERSION}
 fi
 # 
